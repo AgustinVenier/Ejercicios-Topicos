9ca51505df464ddfc10ca740e4c9fd22290a0610
AgustinVenier <agusvenier04@gmail.com>
2025-08-25 11:04:19 -0300

    actualice el archivo funciones.c

--- matematica/Source/funciones.c
+++ matematica/Source/funciones.c
@@ -29,3 +29,61 @@ int ex(int x, int tol)
     }
     return termino;
 }
+//ejercicio 4:no entendi
+//ejercicio 5: hasta que numero calculo la serie fibonacci?
+
+int seno(int x, int tol)
+{
+    int i=1, termino=x, cont=1;
+    while(abs(termino) > tol)
+    {
+        cont++;
+        if(cont%2==0)
+            termino-=(pow(i)/factorial(i));
+        termino+=(pow(i)/factorial(i));
+        i+=2;
+    }
+    return termino;
+}
+
+char clasificacion_naturales(int n)
+{
+    
+}
+
+int producto(int n, int m)
+{
+    int i, resultado=0;
+    if(n<0||m<0)
+        printf("No es un numero natural");
+        return 1;
+    for(i=0;i<m;i++)
+        resultado+=n;
+    return resultado;
+}
+
+//pregutnar ejercicio 9: informo el resto por un lado y por otro informo el cociente?
+
+int suma_primeros(int n)
+{
+    int i, suma;
+    if(n<1)
+        printf("No es un numero natural");
+    for(i=0;i<n;i++)
+        suma+=i;
+    return suma;
+}
+
+int suma_pares(int n)
+{
+    int i, suma;
+    if(n<1)
+        printf("No es un numero natural");
+    for(i=0;i<n;i++)
+    {
+        if(n%2==0)
+            suma+=i;
+    }
+    return suma;
+}
+
